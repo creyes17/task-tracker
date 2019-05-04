f205dfee8a152a1c6d4e8c8e250309b9f6eb0fc2
Christopher R. Reyes <ChrisReyes@alum.mit.edu>
2019-05-04 07:52:55 -0700

    Creating root tasks

--- src/task_tracker/core.clj
+++ src/task_tracker/core.clj
@@ -1,6 +1,28 @@
 (ns task-tracker.core
   (:gen-class))
 
+(defrecord Task [this-numerator
+                 this-denominator
+                 next-numerator
+                 next-denominator
+                 num-children])
+
+(defn create-root
+  "Creates a new root `task-tracker.core/Task`.
+  The resulting `task-tracker.core/Task` will have `(= (/ :this-numerator :this-denominator) value)`"
+  [value]
+  (map->Task {:this-numerator value
+              :this-denominator 1
+              :next-numerator (inc value)
+              :next-denominator 1
+              :num-children 0}))
+
+(defn add-child
+  "Adds a child to `root`."
+  [root]
+  (println "TODO: Implement")
+  {:root root :child nil})
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- test/task_tracker/core_test.clj
+++ test/task_tracker/core_test.clj
@@ -2,6 +2,18 @@
   (:require [clojure.test :refer :all]
             [task-tracker.core :refer :all]))
 
-(deftest a-test
-  (testing "TODO: Add tests"
-    (is (= 1 1))))
+(deftest test-create-root
+  (testing "Can create root"
+    (doseq [value [1 2]]
+      (let [root (create-root value)]
+        (is (= (:this-numerator root) value) "Numerator should be the supplied value")
+        (is (= (:this-denominator root) 1) "Denominator for root tasks is always 1")
+        (is (= (:next-numerator root) (inc value)) "Numerator for next root Task should be incremented")
+        (is (= (:next-denominator root) 1) "Denominator for root tasks is always 1")
+        (is (= (:num-children root) 0) "Root tasks start with no children")))))
+
+
+(deftest test-add-child
+  (testing "TODO: add test for add child"
+    (let [root (create-root 1)]
+      (is (not (nil? (add-child root)))))))
