07f7053e04162f6b0b5854d965e1d0bf4d911337
Christopher R. Reyes <ChrisReyes@alum.mit.edu>
2019-05-18 11:41:29 -0700

    do-select-query function

--- src/task_tracker/persistence.clj
+++ src/task_tracker/persistence.clj
@@ -4,17 +4,26 @@
             [clojure.java.jdbc :as jdbc]
             [cognitect.aws.client.api :as aws]))
 
+; TODO: Warn if environment variable not present
+; TODO: Look into updating these values periodically and/or in response to a trigger
+(def ^:private db-config (let [credentials-secret (System/getenv "C17_TASKTRACKER_POSTGRES_SECRET")
+                               secrets-manager-client (aws/client {:api :secretsmanager})
+                               secretResponse (aws/invoke secrets-manager-client
+                                                          {:op :GetSecretValue
+                                                           :request {:SecretId credentials-secret}})
+                               secret (json/decode (get secretResponse :SecretString) true)]
+                               (assoc (select-keys secret [:dbname :host :password :port])
+                                                   :dbtype "postgres"
+                                                   :user (get secret :username))))
+
+(defn- do-select-query
+  "Executes the given select query"
+  [prepared-statement]
+  (jdbc/query db-config prepared-statement))
+
 (defn debug-postgres-connection
   "Just runs a simple query to confirm we can connect to postgres"
   []
   ; Get credentials
-  (let [credentials-secret (System/getenv "C17_TASKTRACKER_POSTGRES_SECRET")
-        secrets-manager-client (aws/client {:api :secretsmanager})
-        secretResponse (aws/invoke secrets-manager-client
-                                   {:op :GetSecretValue
-                                    :request {:SecretId credentials-secret}})
-        secret (json/decode (get secretResponse :SecretString) true)
-        database (assoc (select-keys secret [:dbname :host :password :port])
-                        :dbtype "postgres"
-                        :user (get secret :username))]
-    (jdbc/query database "select * from hierarchy")))
+  (let [statement "select * from hierarchy"]
+    (do-select-query statement)))
