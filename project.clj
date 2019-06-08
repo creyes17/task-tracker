56b6f588cf5938ff0f69c2add4f78dc06bd87cf5
Christopher R. Reyes <ChrisReyes@alum.mit.edu>
2019-06-08 07:22:40 -0700

    Added license notices

--- project.clj
+++ project.clj
@@ -1,3 +1,20 @@
+;; Copyright (C) 2019  Christopher R. Reyes
+;;
+;; This file is part of Task Tracker.
+;;
+;; Task Tracker is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; Task Tracker is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with Task Tracker.  If not, see <https://www.gnu.org/licenses/>.
+
 (defproject task-tracker "0.1.0-SNAPSHOT"
   :description "Keeps track of task estimates and subtasks."
   :url "http://www.github.com/creyes17/task-tracker"
