b420bef99a5b25927f8e4077ba10fa1382440193
Christopher R. Reyes <ChrisReyes@alum.mit.edu>
2019-11-02 13:55:08 -0700

    Added basic server. Need to set up with docker-compose

--- src/dev/chrisreyes/task_tracker/core.clj
+++ src/dev/chrisreyes/task_tracker/core.clj
@@ -23,85 +23,94 @@
   end-users to interact with the system by creating, reading,
   updating, and deleting tasks."
   (:gen-class)
-  (:require [dev.chrisreyes.task-tracker.hierarchy :as hierarchy]
-            [dev.chrisreyes.task-tracker.persistence :as persistence]))
+  (:require [clojure.data.json :as json]
+            [compojure.core]
+            [dev.chrisreyes.task-tracker.hierarchy :as hierarchy]
+            [dev.chrisreyes.task-tracker.persistence :as persistence]
+            [org.httpkit.server :refer [run-server]]))
 
 
-(defn -db-config
+(defn- db-config
   "Gets the default db-config"
   []
   (persistence/get-config
     (persistence/get-secret-from-aws
       (persistence/get-credentials-secret))))
 
-(defn -add-subtasks
-  "Adds n children tasks to a parent task"
+(defn- add-subtasks
+  "Adds n children tasks to a parent ::task"
   ([root-task]
-   (-add-subtasks root-task 3))
+   (add-subtasks root-task 3))
   ([root-task n]
-   (let [db-config (-db-config)]
-     (reduce (fn [root i] (let [child-hierarchy-data (hierarchy/add-child
-                                                      (:hierarchy-node root))
-                                child-hierarchy (:child child-hierarchy-data)
-                                updated-root-hierarchy (:hierarchy-node child-hierarchy-data)
-                                child-task {:hierarchy-node child-hierarchy
-                                            :issue-link (str (inc i)
-                                                             "th Child of "
-                                                             n
-                                                             " for task "
-                                                             (:task-id root))
-                                            :estimated-time-minutes (* 10 (inc i))}]
-                            (persistence/save-task db-config child-task (System/getenv "USER"))
-                            (assoc root :hierarchy-node updated-root-hierarchy)))
-             root-task
-             (range n)))))
+   (reduce (fn [root i] (let [child-hierarchy-data (hierarchy/add-child
+                                                    (:hierarchy-node root))
+                              child-hierarchy (:child child-hierarchy-data)
+                              updated-root-hierarchy (:hierarchy-node child-hierarchy-data)
+                              child-task {:hierarchy-node child-hierarchy
+                                          :issue-link (str (inc i)
+                                                           "th Child of "
+                                                           n
+                                                           " for task "
+                                                           (:task-id root))
+                                          :estimated-time-minutes (* 10 (inc i))}]
+                          (persistence/save-task (db-config) child-task (System/getenv "USER"))
+                          (assoc root :hierarchy-node updated-root-hierarchy)))
+           root-task
+           (range n))))
 
-(defn -add-nested-subtasks
-  "Adds n successive subtasks to a task such that each subtask is a parent of the next subtask added"
+(defn- add-nested-subtasks
+  "Adds n successive subtasks to a ::task such that each subtask is a parent of the next subtask added"
   ([root-task]
-   (-add-nested-subtasks root-task 3))
+   (add-nested-subtasks root-task 3))
   ([root-task n]
-   (let [db-config (-db-config)]
-     (reduce (fn [root i] (let [child-hierarchy-data (hierarchy/add-child
-                                                      (:hierarchy-node root))
-                                child-hierarchy (:child child-hierarchy-data)
-                                child-task {:hierarchy-node child-hierarchy
-                                            :issue-link (str (inc i)
-                                                             "th Recursive Child of "
-                                                             n
-                                                             " for task "
-                                                             (:task-id root-task))
-                                            :estimated-time-minutes (* 10 (inc i))}]
-                            (persistence/save-task db-config child-task (System/getenv "USER"))))
-             root-task
-             (range n)))))
+   (reduce (fn [root i] (let [child-hierarchy-data (hierarchy/add-child
+                                                    (:hierarchy-node root))
+                              child-hierarchy (:child child-hierarchy-data)
+                              child-task {:hierarchy-node child-hierarchy
+                                          :issue-link (str (inc i)
+                                                           "th Recursive Child of "
+                                                           n
+                                                           " for task "
+                                                           (:task-id root-task))
+                                          :estimated-time-minutes (* 10 (inc i))}]
+                          (persistence/save-task (db-config) child-task (System/getenv "USER"))))
+           root-task
+           (range n))))
 
-(defn -create-task-with-subtasks
-  "Creates a single task with n children"
-  ([]
-   (-create-task-with-subtasks 3))
-  ([n]
-   (let [db-config (-db-config)
-         root-numerator (persistence/get-next-root db-config)
-         root-hierarchy (hierarchy/create-root root-numerator)
-         root-task (persistence/save-task db-config
-                                          {:hierarchy-node root-hierarchy
-                                           :issue-link (str "Hello "
-                                                            root-numerator
-                                                            " Parent!")
-                                           :estimated-time-minutes 10}
-                                          (System/getenv "USER"))]
-     (-add-subtasks root-task n))))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (let [db-config (persistence/get-config
-                    (persistence/get-secret-from-aws
-                      (persistence/get-credentials-secret)))
-        this-numerator (persistence/get-next-root db-config)
+(defn- create-next-root-task
+  "Creates and returns a new root-level ::task"
+  []
+  (let [config (db-config)
+        this-numerator (persistence/get-next-root config)
         next-root (hierarchy/create-root this-numerator)
         task {:hierarchy-node next-root
               :issue-link (str "Hello " this-numerator " World")
               :estimated-time-minutes 30}]
-    (persistence/save-task db-config task (System/getenv "USER"))))
+    (persistence/save-task config task (System/getenv "USER"))))
+
+(defn- create-task-with-subtasks
+  "Creates a single ::task with n children"
+  ([]
+   (create-task-with-subtasks 3))
+  ([n]
+   (let [root-task (create-next-root-task)]
+     (add-subtasks root-task n))))
+
+(compojure.core/defroutes backend-api
+  (compojure.core/GET "/" [] "Hello World!\n"))
+
+(defn get-backend-api-port
+  "Gets the port to use for the backend API as an integer. Defaults to 5000"
+  []
+  (let [env-port (System/getenv "C17_TASKTRACKER_BACKEND_API_PORT")]
+    ; If the environment variable is defined, it will be a string.
+    ; We want to convert it to an integer.
+    ; Otherwise, just return the default.
+    (if (some? env-port)
+      (Integer/parseInt env-port)
+      5000)))
+
+(defn -main
+  "Starts a backend webserver to handle API requests for working with tasks"
+  [& args]
+  (run-server backend-api {:port (get-backend-api-port)}))
